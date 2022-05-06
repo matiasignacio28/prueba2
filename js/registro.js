0e403fa6ba7c92992be8da42086f290df2735215
angelomaluenda <ang.maluenda@duocuc.cl>
2022-05-06 01:57:39 -0400

    validaciones en registro

--- js/registro.js
+++ js/registro.js
@@ -5,16 +5,17 @@ $(document).ready(()=> {
         evento.preventDefault();
         $('.error').hide();
         
-
+        
 
         let contrasena =$('#contrasena').val();
         let contrasenaConfirmar = $('#contrasenaConfirmar').val();
     
-        if (contrasena !== contrasenaConfirmar){
-            $('#contrasenaNoIguales').show();
+        if (contrasena !== contrasenaConfirmar||(contrasena).length<=3){
+            $('#contrasenaIncorrecta').show();
             return;
     
         }
+      
        
         alert("El registro se realizo correctamente");
         $('input[type="text"]').val('');
@@ -24,4 +25,47 @@ $(document).ready(()=> {
     })
 
 });
+
+
+const filtrar = ()=>{ 
+    let contrasena =$('#contrasena').val();
+    let contrasenaConfirmar = $('#contrasenaConfirmar').val();
+    if (contrasena == contrasenaConfirmar&&(contrasena).length>3){
+        $('#contrasenaCorrecta').show()&&$('#contrasenaIncorrecta').hide()&&$('#detalle').hide();
+        return;  
+    }
+    else
+        $('#contrasenaCorrecta').hide();
+        return;
+
+
+}
+
+
+
+
+
+
+contrasenaConfirmar.addEventListener('keyup',filtrar)
+contrasena.addEventListener('keyup',filtrar)
+
+$(function(){
+    var esOculto = false;
+    $('#contrasenaIncorrecta').click(function(){
+        if (esOculto){
+            $('#detalle').show();
+            esOculto=false;
+
+        }else{
+            $('#detalle').hide();
+            esOculto=true;
+        }
+
+    })
+
+
+})
+
+
+
 
